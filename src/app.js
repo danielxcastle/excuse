373df0e758b062a0fc7e5082640154ec300a4cef
danielxcastle <138261809+danielxcastle@users.noreply.github.com>
2023-07-10 17:55:26 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let excuse = document.getElementById("excuse");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let whoRand = Math.floor(Math.random() * who.length);
+  let actionRand = Math.floor(Math.random() * action.length);
+  let whatRand = Math.floor(Math.random() * what.length);
+  let whenRand = Math.floor(Math.random() * when.length);
+  excuse.innerHTML =
+    who[whoRand] +
+    " " +
+    action[actionRand] +
+    " " +
+    what[whatRand] +
+    " " +
+    when[whenRand];
   console.log("Hello Rigo from the console!");
 };
